764d0f09668b6117fdd024c4a33e6022b7a8c60d
tkapusniakaxxvbf <tkapusniak@stcharlessd.org>
2014-09-26 04:30:28 -0500

    modified:   Craps_Roller.py

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -1,17 +1,22 @@
 # Craps Roller
 # Demonstrates random number generation
 # Original Author: Michael Dawson
-# Last Edited: Thorin Schmidt
-# Date Edited: 9/26/2014
+# Last Edited: Thorin Schmidt or Tyler Kapusniak
+# Date Edited: 9/26/2014 9/28/2014
 
 impart random
 
 # generate random numbers 1 - 6
 die1 = random.randint(1, 6) 
-die2 = random.randrange(6)
+die2 = random.randint(1, 6)
 
 total = 'die1' + 'die2'
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
-input("\n\nPress the enter key to exit.")
+input("Press the enter key to exit.")
+
+
+
+
+#Sorry, I never got to doing random numbers so I don't really know how to fix this.
